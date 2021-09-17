0c17b6fd50727cdf0b7aeee3846b88492b76e0f7
Patriosx <patriosweb@gmail.com>
2021-09-17 17:37:49 +0100

    nombre corregido

--- models/destino.js
+++ models/destino.js
@@ -1,35 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const destinoSchema = new Schema({
-  nombre: {
-    type: String,
-    required: true
-  },
-  descripcion: {
-    type: String,
-    required: true
-  },
-  localizacion: {
-    lat: {
-      type: Number,
-    },
-    lng: {
-      type: Number,
-    },
-  },
-  direccion: {
-    type: String,
-    required: true
-  },
-  creador: {
-    // type: String,
-    // required: true
-    type: mongoose.Types.ObjectId,
-    required: true,
-    ref: 'Usuario'
-  }
-})
+	nombre: {
+		type: String,
+		required: true,
+	},
+	descripcion: {
+		type: String,
+		required: true,
+	},
+	localizacion: {
+		lat: {
+			type: Number,
+		},
+		lng: {
+			type: Number,
+		},
+	},
+	direccion: {
+		type: String,
+		required: true,
+	},
+	creador: {
+		type: mongoose.Types.ObjectId,
+		required: true,
+		ref: "Usuario",
+	},
+});
 
-module.exports = mongoose.model('Destino', destinoSchema);
+module.exports = mongoose.model("Destino", destinoSchema);
